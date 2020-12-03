9b9bc183819a3cf51dcc24c9aab818406cfcf809
konopek1 <konopek68@gmail.com>
2020-12-03 16:30:25 +0100

    aoc 1-2-3

--- src/aoc/aoc2.clj
+++ src/aoc/aoc2.clj
@@ -1,5 +1,40 @@
 (ns aoc2)
 (require '[clojure.string :as str])
 
+(defn 
+  parse-line [line]
+  (let [[times letters passwd] (str/split line #" ")]
+    (let [[min max] (str/split times #"-")
+         [letter] (first (str/split letters #":"))
+         ]
+      {:min (Integer/parseInt min) :max (Integer/parseInt max) :letter letter :passwd passwd}
+      )
+    )
+  )
 
-(def input (map #(Integer/parseInt %) (str/split (slurp "resources/input1.txt") #"\n")))
+(def 
+  input 
+  (map parse-line (str/split (slurp "resources/input2.txt") #"\n"))) 
+
+(defn pass?
+  [record]
+  (<=
+   (:min record)
+   (count  (filter #(= % (:letter record)) (:passwd record)))
+   (:max record))
+  )
+
+(defn pass2?
+  [record]   
+    (count (filter #(= % (:letter record))
+            [(nth (:passwd record) (dec (:min record)) nil) (nth (:passwd record) (dec (:max record)) nil)]))
+  )
+
+(count input)
+
+(pass2? (parse-line "1-3 a: abcde"))
+
+(count(filter #(= % true) (map pass? input)))
+
+(count (filter #(= % 1) (map pass2? input)))
+(map pass2? input)
---
+++ src/aoc/aoc3.clj
@@ -0,0 +1,36 @@
+(ns aoc3)
+(require '[clojure.string :as str])
+
+(defn
+  parse-line
+  [line]
+  {:trees (map-indexed (fn [idx item] (when (= item \#) idx)) line)
+   :dots (map-indexed (fn [idx item] (when (= item \.) idx)) line)})
+
+(def
+  input
+  (map parse-line (str/split (slurp "resources/input3.txt") #"\n")))
+
+(def width (count (:trees (first input))))
+
+(defn
+  check-if-tree
+  [row col path]
+  (if (some #{(mod col width)}  (:trees (nth path row))) 1 0))
+
+
+(defn traverse
+  [input [col-step row-step]]
+  (loop [path input row 0 col 0 trees 0]
+    (print row-step " " col-step "\n")
+    (if (<= (+ row row-step) (dec (count path)))
+      (recur path (+ row row-step) (+ col col-step) (+ trees (check-if-tree row col path)))
+      trees)))
+
+(def slops `([1 1]
+            [3 1]
+            [5 1]
+            [7 1]
+            [1 2]))
+
+(apply * (map #(traverse input %) slops))
